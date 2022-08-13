0c9f1d1822a601dfc17116d5eac21ead174c4474
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-13 11:14:28 +0300

    определили палиндром или нет

---
+++ zadacha_19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+
+void num (string number)
+{
+      if ( number [0] == number[4] || number [1] == number[3])
+      {
+            Console.WriteLine("число -> палиндром." );
+      }
+      else Console.WriteLine("число -> не палиндром.");
+}
+if (number.Length==5)
+{
+      num(number);
+}
